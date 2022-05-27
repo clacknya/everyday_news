7df4fa2c9b644e9254d73a92e24be7928a50c08f
clacknya <clacknya@localhost>
2022-05-27 20:24:33 +0800

    reconstituted to aiohttp

--- everyday_news.py
+++ everyday_news.py
@@ -1,46 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 __version__ = '0.1.0'
+
 import os
-#from PIL import Image, ImageFont, ImageDraw
-#from hoshino import util, R
+import aiohttp
+import aiofiles
+
 from hoshino import Service
-import requests as req
-import json
+from hoshino.typing import CQEvent, MessageSegment
 
-sv = Service('everydayNews', visible= True, enable_on_default= True, bundle='everydayNews', help_='''
-每日简报，请发送“每日简报、报哥或每日新闻”
+sv = Service('everydayNews', visible=True, enable_on_default=False, help_='''
+[每日简报] 请发送“每日简报、报哥或每日新闻”
 '''.strip())
+
 path = os.path.dirname(__file__)
-def getPicUrl():
-    getObject = req.get('http://api.soyiji.com//news_jpg')
-    if getObject.status_code != 200:
-        sv.logger.warning('URL获取失败')
-        return()
-    else:
-        sv.logger.info('图片URL获取成功')
-    return(json.loads(getObject.text)['url'])
 
-def getImg():
-    getObject = req.get(
-        headers={'Referer':'safe.soyiji.com'},
-        url=getPicUrl()
-    )
-    if getObject.status_code != 200:
-        sv.logger.warning('图片获取失败')
-        return()
-    else:
-        sv.logger.info('图片获取成功')
-    with open(os.path.join(path, "tmp.jpg"),'wb') as image:
-        image.write(getObject.content)
+async def getImgUrl() -> str:
+	try:
+		async with aiohttp.ClientSession(raise_for_status=True) as session:
+			async with session.get('http://api.soyiji.com//news_jpg') as resp:
+				ret = await resp.json()
+	except Exception as e:
+		sv.logger.error('图片URL获取失败')
+	else:
+		sv.logger.info('图片URL获取成功')
+		return ret['url']
 
+async def getImg() -> bytes:
+	try:
+		async with aiohttp.ClientSession(raise_for_status=True) as session:
+			async with session.get(await getImgUrl(), headers={'Referer': 'safe.soyiji.com'}) as resp:
+				ret = await resp.read()
+	except Exception as e:
+		sv.logger.error('图片获取失败')
+	else:
+		sv.logger.info('图片获取成功')
+		return ret
 
-@sv.on_fullmatch(('每日简报','报哥','每日新闻'))
-async def news(bot,ev):
-    getImg()
-    tmppath = os.path.join(path, "tmp.jpg")
-    await bot.send(ev, f"[CQ:image,file=file:///{tmppath}]", at_sender=True)
+@sv.on_fullmatch(('每日简报', '报哥', '每日新闻'))
+async def news(bot, ev: CQEvent):
+	try:
+		async with aiofiles.tempfile.NamedTemporaryFile('wb', delete=False) as f:
+			await f.write(await getImg())
+		os.chmod(f.name, 0o644)
+		await bot.send(ev, MessageSegment.image(f"file:///{f.name}"), at_sender=True)
+	finally:
+		if os.path.isfile(f.name):
+			os.remove(f.name)
 
-@sv.scheduled_job('cron', hour = '9')
+@sv.scheduled_job('cron', hour='9')
 async def news_scheduled():
-    getImg()
-    tmppath = os.path.join(path, "tmp.jpg")
-    await sv.broadcast(f"[CQ:image,file=file:///{tmppath}]",'auto_send_news_message', 2)
+	try:
+		async with aiofiles.tempfile.NamedTemporaryFile('wb', delete=False) as f:
+			await f.write(await getImg())
+		os.chmod(f.name, 0o644)
+		await sv.broadcast(MessageSegment.image(f"file:///{f.name}"), 'auto_send_news_message', 2)
+	finally:
+		if os.path.isfile(f.name):
+			os.remove(f.name)
